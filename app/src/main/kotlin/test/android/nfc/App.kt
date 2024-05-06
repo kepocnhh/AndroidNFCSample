3b63bb4755d08e74e63163185ed9be1f309916cd
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-06 15:38:46 +0300

    init compose

---
+++ app/src/main/kotlin/test/android/nfc/App.kt
@@ -0,0 +1,40 @@
+package test.android.nfc
+
+import android.app.Application
+import androidx.activity.OnBackPressedDispatcher
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import test.android.nfc.util.compose.LocalOnBackPressedDispatcher
+import test.android.nfc.util.compose.toPaddings
+
+internal class App : Application() {
+    object Theme {
+        private val LocalInsets = staticCompositionLocalOf<PaddingValues> { error("No insets!") }
+
+        val insets: PaddingValues
+            @Composable
+            @ReadOnlyComposable
+            get() = LocalInsets.current
+
+        @Composable
+        fun Composition(
+            onBackPressedDispatcher: OnBackPressedDispatcher,
+            content: @Composable () -> Unit,
+        ) {
+            CompositionLocalProvider(
+                LocalOnBackPressedDispatcher provides onBackPressedDispatcher,
+                LocalInsets provides LocalView.current.rootWindowInsets.toPaddings(),
+                content = content,
+            )
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        // todo
+    }
+}
---
+++ app/src/main/kotlin/test/android/nfc/MainActivity.kt
@@ -0,0 +1,20 @@
+package test.android.nfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.platform.ComposeView
+
+internal class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = ComposeView(this)
+        setContentView(view)
+        view.setContent {
+            App.Theme.Composition(
+                onBackPressedDispatcher = onBackPressedDispatcher,
+            ) {
+                // todo
+            }
+        }
+    }
+}
